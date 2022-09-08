645afb05c9f1554a28925c946e736082ee59f8cb
Ben Skoglund <ben.skoglund@live.se>
2022-09-08 18:55:49 +0200

    Fixed broken route link and added lazy load scripts

--- public/assets/bodyclass.js
+++ public/assets/bodyclass.js
@@ -1,1 +1,23 @@
-$('body').toggleClass("veckansbg");
+$("body").toggleClass("veckansbg");
+
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyBackgrounds = [].slice.call(document.querySelectorAll(".veckansbg"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyBackgroundObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          lazyBackgroundObserver.unobserve(entry.target);
+        }
+      });
+    });
+
+    lazyBackgrounds.forEach(function (lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground);
+    });
+  }
+});
--- public/assets/indexbody.js
+++ public/assets/indexbody.js
@@ -1,1 +1,22 @@
-$('body').toggleClass("indexbody");
+$("body").toggleClass("indexbody");
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyBackgrounds = [].slice.call(document.querySelectorAll(".hero"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyBackgroundObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          lazyBackgroundObserver.unobserve(entry.target);
+        }
+      });
+    });
+
+    lazyBackgrounds.forEach(function (lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground);
+    });
+  }
+});
